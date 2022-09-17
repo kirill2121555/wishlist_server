8d65111ace2536d8eff4696fc92cdd14bfb9c6ef
kirill2121555 <kirill2121555@gmail.com>
2022-09-17 17:34:59 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { config } = require('dotenv')
-require('dotenv').config()
 const mongoose = require('mongoose')
 const routers = require('./routers/router')
-const PORT = process.env.PORT
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const { config } = require('dotenv')
+require('dotenv').config()
+
+const PORT = process.env.PORT
 
 const app = express()
 
@@ -15,13 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: 'https://wishlist-client.vercel.app/' }));
 
-
-
 app.use('/', routers)
 
 const start = async () => {
     try {
-        await mongoose.connect(process.env.DB)
+        await mongoose.connect('mongodb+srv://wishlistdb:fpJvnypWHGczSktz@cluster0.0b8yb4c.mongodb.net/?retryWrites=true&w=majority')
         app.listen(PORT, () => console.log('cool'))
 
     } catch (e) {
